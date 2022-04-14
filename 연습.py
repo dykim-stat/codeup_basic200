6b3c4c12f6f94c39e43beafec0d33a8888662837
dykim-stat <haedoji96@gmail.com>
2022-04-14 16:23:28 +0900

    Add files

--- 연습.py
+++ 연습.py
@@ -1,8 +1,30 @@
-r, g, b = input().split(' ')
-r, g, b = int(r), int(g), int(b)
+M = [[0 for j in range(10)] for i in range(10)]
+for i in range(len(M)):
+    value = list(map(int, input().split(' ')))
+    M[i] = value
 
-for i in range(r):
-    for j in range(g):
-        for k in range(b):
-            print(i,j,k)
-print(r*g*b)
+xi, yi = 1, 1
+M[xi][yi] = 9
+
+while True:
+    if M[xi][yi+1] != 1:
+        if M[xi][yi+1]==2:
+            M[xi][yi+1] = 9
+            break
+        M[xi][yi+1] = 9
+        xi, yi = xi, yi+1
+
+    elif M[xi+1][yi] != 1:
+        if M[xi+1][yi]==2:
+            M[xi+1][yi] = 9
+            break
+        M[xi+1][yi] = 9
+        xi, yi = xi+1, yi
+
+    else:
+        break
+
+for i in range(10):
+    for j in range(10):
+        print(M[i][j], end=' ')
+    print()
