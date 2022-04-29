986e5c02c9323395f06403cdc184ea189d653b1e
dykim-stat <haedoji96@gmail.com>
2022-04-29 13:00:50 +0900

    Add files

--- 연습장.py
+++ 연습장.py
@@ -1,10 +1,7 @@
-h = int(input())
+n, m = map(int, input().split())
 
-count = 0
-for i in range(h+1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i)+str(j)+str(k):
-                count += 1
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
 
-print(count)
+print(graph)
